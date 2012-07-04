9d319b185a84a908a2092abea411e9ddb588fb54
Calvin Spealman <ironfroggy@gmail.com>
2012-07-04 06:35:12 -0400

    Remove a newpost command from an old project, doesn't work here.

--- fabfile.py
+++ fabfile.py
@@ -9,29 +9,6 @@ from fabric.state import env
 
 env.hosts = ['www.ironfroggy.com']
 
-def newpost(title=None, src=None):
-    if title is None and src is not None:
-        base = os.path.splitext(os.path.basename(src))[0]
-        title = base.replace('-', ' ').title()
-    alphanum = string.letters + string.digits
-    slug = title.lower().replace(' ', '-')
-    slug = ''.join(c for c in slug if c in alphanum + '-')
-    now = datetime.datetime.now()
-    filename = 'content/posts/{}-{}.html'.format(now.strftime('%Y-%m-%d'), slug)   
-    if os.path.exists(filename):
-        print "refusing to overwrite", filename
-    else:
-        with open(filename, 'w') as f:
-            f.write('---\n')
-            f.write('title: {}\n'.format(title))
-            f.write("created: !!timestamp '{}'\n".format(now.strftime('%Y-%m-%d %H:%M:%S')))
-            f.write('status: draft\n')
-            f.write('---\n')
-            f.write('\n')
-            src = os.path.expanduser(src)
-            if src and os.path.exists(src):
-                f.writelines(open(src))
-
 def clean():
     local('find . -name "*.swp" -delete')
 
