0e9915924018aa8b7aad1f1c88c0dd08d3c745b9
Calvin Spealman <ironfroggy@gmail.com>
2012-10-14 23:54:29 -0400

    Fixing up a few project templates to add jules and tracerlib pages

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 import os
+import re
 import datetime
 import string
 
@@ -24,3 +25,32 @@ def update():
     clean()
     local('git pull')
     build()
+
+template = """title: "{title}"
+publish_time: !!timestamp '{now}'
+updated_time: !!timestamp '{now}'
+created_time: !!timestamp '{now}'
+status: published
+type: post
+rnder: jinja2
+template: post.j2
+series: {series}
+content: {content}
+"""
+def new():
+    params = dict(
+        title = raw_input("title: "),
+        key = raw_input("path: "),
+        series = raw_input("series: "),
+        now = datetime.datetime.now().strftime('%Y-%M-%d %h:%m:%s'),
+        content = raw_input("content: "),
+    )
+    if not params['key']:
+        params['key'] = key = title.lower().replace(' ', '-')
+        assert re.match(r'[-a-z/]*', key)
+    else:
+        key = params['key']
+    if not params['content']:
+        open(os.path.join('contents', key) + '.rst', 'w')
+    with open(os.path.join('contents', key) + '.yaml', 'w') as f:
+        f.write(template.format(**params))
